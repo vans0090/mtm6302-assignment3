14ce29c87d4a4e205f887f07913f1ca9087adce4
vanshika <vans0090@algonquinlive.com>
2021-04-01 20:36:00 -0400

    Changes

--- script.js
+++ script.js
@@ -1,60 +1,127 @@
 const $form = document.getElementById('form')
 const $year = document.getElementById('year')
 const $month = document.getElementById('month')
-const $day = document.getElementById('day-container')
+const $day = document.getElementById('day')
 const $submit = document.getElementById('submit')
+const $result = document.getElementById('result')
+const $reset = document.getElementById('reset')
 
 
-//declaring months
-
-const $jan = document.getElementById('m1')
-const $feb = document.getElementById('m2')
-const $mar = document.getElementById('m3')
-const $apr = document.getElementById('m4')
-const $may = document.getElementById('m5')
-const $jun = document.getElementById('m6')
-const $jul = document.getElementById('m7')
-const $aug = document.getElementById('m8')
-const $sep = document.getElementById('m9')
-const $oct = document.getElementById('m10')
-const $nov = document.getElementById('m11')
-const $dec = document.getElementById('m12')
-
-let numberOfDays = []
 $month.addEventListener('click', function (){
 
     function printDays() {
-        if ($month.value == 'jan' || $month.value == 'march' || $month.value == 'may' || $month.value == 'july' || $month.value == 'aug' || $month.value == 'oct' || $month.value == 'dec' ) {
+        if ($month.value == '1' || $month.value == '3' || $month.value == '5' || $month.value == '7' || $month.value == '8' || $month.value == '10' || $month.value == '12' ) {
         
-        numberOfDays.push(` <select name="day" id="day">`)
+            let numberOf31Days = []
             for (let i=1; i <=31; i++){
-                numberOfDays.push(`<option value = ${i} id = ${i}>${i}<option>`)
+                numberOf31Days.push(`<option value = ${i} id = ${i}>${i}</option>`)
             }
-        numberOfDays.push(`</select>`)
-            $day.innerHTML = numberOfDays.join('')
+        
+            $day.innerHTML = numberOf31Days.join('')
 
         }
         
-        else if ($month.value == 'apr' || $month.value == 'june' || $month.value == 'sep' || $month.value == 'nov') {
+        else if ($month.value == '4' || $month.value == '6' || $month.value == '9' || $month.value == '11') {
 
-            numberOfDays.push(` <select name="day" id="day">`)
+            let numberof30Days = []
             for (let i=1; i <=30; i++){
-                numberOfDays.push(`<option value = ${i} id = ${i}>${i}<option>`)
+                numberof30Days.push(`<option value = ${i} id = ${i}>${i}</option>`)
             }
-            numberOfDays.push(`</select>`)
-            $day.innerHTML = numberOfDays.join('')
+            
+            $day.innerHTML = numberof30Days.join('')
         }
 
         else {
         
-        numberOfDays.push(` <select name="day" id="day">`)
+            let numberof28Days = []
+        
         for (let i=1; i <=28; i++){
-            numberOfDays.push(`<option value = ${i} id = ${i}>${i}<option>`)
+            numberof28Days.push(`<option value = ${i} id = ${i}>${i}</option>`)
         }
-        numberOfDays.push(`</select>`)
-        $day.innerHTML = numberOfDays.join('')
+        $day.innerHTML = numberof28Days.join('')
         }
     }
 
         printDays()
 })
+
+
+// get date 
+
+$submit.addEventListener('click', function(event){
+    event.preventDefault()
+
+    $form.style.display = 'none'
+    $result.style.display = 'block'
+    $reset.style.display = 'block'
+
+   setInterval(function (){ 
+
+    const currentDate = new Date()
+    const newDate = new Date($year.value, $month.value - 1, $day.value, 0, 0)
+    const difference = newDate.getTime() - currentDate.getTime()
+
+function toDays(ms) {
+    return Math.floor(ms/ 1000/ 60/ 60/ 24)
+}
+
+function toHours(ms) {
+    const days = toDays(ms)
+    const hours = Math.floor(ms/ 1000/ 60/ 60)
+
+    const remainingHours = hours - (days * 24)
+
+    return remainingHours
+    
+}
+
+function toMinutes(ms) {
+    const days = toDays(ms)
+    const hours = toHours(ms)
+    const minutes = Math.floor(ms/ 1000/ 60)
+
+    const remainingMinutes = minutes - ((days * 24* 60)+(hours*60))
+
+    return remainingMinutes
+    
+}
+
+ function toSeconds(ms) {
+    const days = toDays(ms)
+    const hours = toHours(ms)
+    const minutes = toMinutes(ms)
+    const seconds = Math.floor(ms/ 1000)
+
+    const remainingSeconds = seconds - ((days * 24* 60*60)+(hours*60*60)+(minutes*60))
+
+    return remainingSeconds
+}
+
+
+$result.innerHTML = `<h3 class='result'>Time Remaining</h3>
+                    <p><span>${toDays(difference)}</span>:<span> ${toHours(difference)} </span> : <span> ${toMinutes(difference)} </span> : <span> ${toSeconds(difference)}</span></p>`
+                }, 1000)
+
+                setTimeout(function () {
+                    $reset.innerHTML = `<button type='submit' id='reset'>Select a new date</button>`
+                }, 1000)
+                
+                
+                $reset.addEventListener('click', function(){
+                    $reset.style.display = 'none'
+                    $result.style.display = 'none'
+                
+                    document.getElementById ('form').reset();
+                    $form.style.display = 'block'
+                    
+                })
+              
+            })
+
+
+
+
+
+
+
+
